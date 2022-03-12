1e1ebcba39f2a49f0378488ce7d79eae2eb9389b
楊嘉翔 <81738019+Yang-Jiaxiang@users.noreply.github.com>
2022-03-13 07:26:57 +0800

    Edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import CreatePost from "./page/CreatePost";
+import Category from "./page/Category";
+import PostList from "./page/PostList";
+import Login from "./page/Login";
+import EditPost from "./page/EditPost";
+import { Routes, Route } from "react-router-dom";
+import PageLayout from "./PageLayout";
+import Signout from "./component/Signout";
+import User from "./page/User";
+import CreateUser from "./page/CreateUser";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Routes>
+            <Route index element={<Login />} />
 
+            <Route path="/" element={<PageLayout />}>
+                <Route path="/CreatePost" element={<CreatePost />} />
+                <Route path="/PostList" element={<PostList />} />
+                <Route path="/Category" element={<Category />} />
+                <Route path="/EditPost" element={<EditPost />} />
+                <Route path="/User" element={<User />} />
+                <Route path="/CreateUser" element={<CreateUser />} />
+                <Route path="/Signout" element={<Signout />} />
+            </Route>
+
+            <Route path="*" element={<Login />} />
+        </Routes>
+    );
+}
 export default App;
